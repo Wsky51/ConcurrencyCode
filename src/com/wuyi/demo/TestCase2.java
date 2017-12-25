34d1ec16014010e932e5b6b11283449194bfbde1
stanley.wu <stanley.wu@ericsson.com>
2017-12-25 21:28:16 +0800

    add a new class testcase2

---
+++ src/com/wuyi/demo/TestCase2.java
@@ -0,0 +1,13 @@
+package com.wuyi.demo;
+
+public class TestCase2 {
+	public static void main(String[] args) {
+		System.out.println("dasdas");
+	}
+	public void abc() {
+		System.out.println("abc");
+	}
+	public void bcd() {
+		System.out.println("abc");
+	}
+}
