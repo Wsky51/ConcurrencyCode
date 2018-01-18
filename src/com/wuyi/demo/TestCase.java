b0be5c38a3457eedaca6266b5ac5857855befcaf
stanley.wu <stanley.wu@ericsson.com>
2018-01-18 10:00:02 +0800

    change the testcase

--- src/com/wuyi/demo/TestCase.java
+++ src/com/wuyi/demo/TestCase.java
@@ -2,12 +2,6 @@ package com.wuyi.demo;
 
 public class TestCase {
 	public static void main(String[] args) {
-		System.out.println("feature1");
-	}
-	public void abc() {
-		System.out.println("abc");
-	}
-	public void bcd() {
-		System.out.println("abc");
+		System.out.println("dasdas");
 	}
 }
